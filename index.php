f294c80ec591f8452961c052c49f14c7e75e8bb4
fsy2001 <fsy2001@outlook.com>
2022-12-31 00:58:28 +0800

    impl: basic style

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php
-require 'query.php';
-?>
+<?php require 'query.php'; ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <link rel="icon" type="image/png" sizes="16x16" href="https://www.fduhole.com/img/icons/favicon-16x16.png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="module" src="https://md-block.verou.me/md-block.js" defer></script>
     <title>旦夕年终总结报告</title>
 </head>
 <body>
@@ -18,16 +18,18 @@ require 'query.php';
 <?php if ($total_hole_num['total'] > 0): ?>
     <p>
         本学期的你，
-        一共创建了<?php echo $total_hole_num['total']?>个洞
+        一共创建了<?php echo $total_hole_num['total']?>个洞，
         其中<?php echo $total_hole_reply_num['total']?>个收到了其他洞友的回复。
     </p>
     <p>
         其中，获得回复最多的洞是#<?php echo $highest_reply_hole['id']?>，
         共<?php echo $highest_reply_hole['reply']?>条回复，内容为：
     </p>
-    <p>
-        <?php echo $highest_reply_hole['content']?>
-    </p>
+    <blockquote>
+        <md-block>
+            <?php echo $highest_reply_hole['content']?>
+        </md-block>
+    </blockquote>
 
     <p>帖子被收藏数：<?php echo $highest_fav_num['total']?></p>
 <?php else: ?>
@@ -38,9 +40,11 @@ require 'query.php';
     <p>总共的回帖数量为：<?php echo $total_reply_num['total']?></p>
     <?php if ($highest_like_num): ?>
         <p>最高点赞为##<?php echo $highest_like_num['floor_id']?>，点赞数为：<?php echo $highest_like_num['likes'] ?>，内容为：</p>
-        <p>
-            <?php echo $highest_like_num['content']?>
-        </p>
+        <blockquote>
+            <md-block>
+                <?php echo $highest_like_num['content']?>
+            </md-block>
+        </blockquote>
         <p>你总共获得了<?php echo $total_like['likes']?>个赞</p>
     <?php else: ?>
         <p>你没被点过赞，太可惜了</p>
@@ -61,7 +65,7 @@ require 'query.php';
 
 <p>
     你一共举报了<?php echo $report_num['total']?>次，
-    其中<?php echo $report_delete_num['total']?>帖子个被删了
+    其中<?php echo $report_delete_num['total']?>个帖子被删了
 </p>
 
 
