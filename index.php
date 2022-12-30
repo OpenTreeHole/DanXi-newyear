6590988f6a9b88884a19eac64dc7b1b5379f8fc6
fsy2001 <fsy2001@outlook.com>
2022-12-30 21:55:46 +0800

    fix: conditionalize when user didn't receive any likes

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ require 'query.php';
     </p>
     <p>
         其中，获得回复最多的洞是#<?php echo $highest_reply_hole['id']?>，
-        共<?php echo $highest_reply_hole['reply']?>，内容为：
+        共<?php echo $highest_reply_hole['reply']?>条回复，内容为：
     </p>
     <p>
         <?php echo $highest_reply_hole['content']?>
@@ -36,11 +36,16 @@ require 'query.php';
 
 <?php if ($total_reply_num['total'] > 0): ?>
     <p>总共的回帖数量为：<?php echo $total_reply_num['total']?></p>
-    <p>最高点赞为##<?php echo $highest_like_num['floor_id']?>，点赞数为：<?php echo $highest_like_num['likes'] ?>，内容为：</p>
-    <p>
-        <?php echo $highest_like_num['content']?>
-    </p>
-    <p>你总共获得了<?php echo $total_like['likes']?>个赞</p>
+    <?php if ($highest_like_num): ?>
+        <p>最高点赞为##<?php echo $highest_like_num['floor_id']?>，点赞数为：<?php echo $highest_like_num['likes'] ?>，内容为：</p>
+        <p>
+            <?php echo $highest_like_num['content']?>
+        </p>
+        <p>你总共获得了<?php echo $total_like['likes']?>个赞</p>
+    <?php else: ?>
+        <p>你没被点过赞，太可惜了</p>
+    <?php endif; ?>
+    
     <p>
         你最关注的一个帖子是#<?php echo $most_focused_post['hole_id'] ?>，
         你在这个帖子下回复了<?php echo $most_focused_post['reply']?>条内容
