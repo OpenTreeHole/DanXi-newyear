f7776b3616a703fdc69d3b8d4b5212fe08fb79d0
ppolariss <2673986789@qq.com>
2025-01-22 17:53:36 +0800

    fix: add danke conn

--- src/data/connect_db.php
+++ src/data/connect_db.php
@@ -12,7 +12,14 @@ $auth_conn = new mysqli(getenv("AUTH_DB_HOST"),
     getenv("AUTH_DB_USER"),
     getenv("AUTH_DB_PASSWORD"),
     "auth");
-
 if ($auth_conn->connect_error) {
+    die("连接数据库失败");
+}
+
+$danke_conn = new mysqli(getenv("DANKE_DB_HOST"),
+    getenv("DANKE_DB_USER"),
+    getenv("DANKE_DB_PASSWORD"),
+    "danke");
+if ($danke_conn->connect_error) {
     die("连接数据库失败");
 }
